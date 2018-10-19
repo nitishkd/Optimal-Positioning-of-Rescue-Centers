75afad1c620cc8909d714a5974ce49eb1de63604
amit tiwari <amit.tiwari@students.iiit.ac.in>
2018-10-19 18:54:50 +0530

    code to get lat and lng from place name and height from this lat , lng added

--- GoogleMapsInteraction.py
+++ GoogleMapsInteraction.py
@@ -16,10 +16,29 @@ class GoogleMaps():
 		result = response.json()
 		if(result["status"] == 'OK'):
 			return	float(result['results'][0]['elevation'])
+	def getLatLngFromText(self, query): # it accepts a place name and gets lat,lng and height from it
+		api_key = 'AIzaSyDM8Ns40uQHDt6M51vYHIx11ncTSj84VNo'
+		url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+		r = requests.get(url + 'input=' + query + '&inputtype=textquery'+
+		                        '&key=' + api_key) 
+		x = r.json()
+		y = x['results'] 
+
+		for i in range(len(y)): 
+		    print(y[i]['geometry']['location']['lat'] , " " , y[i]['geometry']['location']['lng'])
+
+		if(len(y) != 0):
+			lat=y[0]['geometry']['location']['lat']
+			lng=y[0]['geometry']['location']['lng']
+			print lat , lng
+			height = self.GetElevation(lat, lng)
+		else:
+			print "nothing"
+		return height
 
 
 if(__name__ == "__main__"):
-
 	obj = GoogleMaps();
 	height = obj.GetElevation(39.7391536, -104.9847034)
 	print (height)
