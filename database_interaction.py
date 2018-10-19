02458631a06fd42701bc34c20a2954d895cb5a19
Aman Joshi <amanjoshi668@gamil.com>
2018-10-20 01:32:41 +0530

    message.py improved

--- database_interaction.py
+++ database_interaction.py
@@ -1,11 +1,29 @@
 import sqlite3
-conn = sqlite3.connect('app.db')
 def insert(table_name, **kargs):
     #something
     return
 
 def get_emergency_contact(mobile_number):
-    cur = conn.cursor()
-    emergency_contact = cur.execute("SELECT emergency_contact FROM users WHERE mobile_number = %s" %mobile_number)
+    conn = sqlite3.connect('app.db')
+    cursor = conn.execute("SELECT emergency_contact FROM users WHERE mobile_number = %s" %mobile_number)
+    for row in cursor:
+        emergency_contact = row[0]
+    conn.close()
     return emergency_contact
     
+def get_name_location(mobile_number):
+    conn = sqlite3.connect('app.db') 
+    cursor = conn.execute("SELECT name, address FROM users WHERE mobile_number = %s" %mobile_number)
+    result = "Your Friend "
+    for row in cursor:
+        result = result + "Name: " + row[0]
+        result = result + " M.No. " + mobile_number
+    
+    result += "Is struck at :"
+
+    cursor = conn.execute("SELECT lattitude, longitude FROM affected_people WHERE mobile_number = %s" %mobile_number)
+    
+    for row in cursor:
+        result = result + " location " + row[0] + ":" + row[1]
+    
+    return result
--- message.py
+++ message.py
@@ -1,35 +1,62 @@
 from twilio.rest import Client
 import database_interaction
-import
+import GoogleMapsInteraction
+
 # Your Account Sid and Auth Token from twilio.com/console
 class Message:
     ACCOUNT_SID = 'ACb77c89f64c5ce3be57484c2b2ddf0e1d'
     AUTH_TOKEN = '8fa22daceff8e23d181efc73e1312d69'
     MOBILE_NUMBER = '+17722131082'
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    
-    def __init__(self):
-        self.body = ""
+    seperator = "####"
+
+
+    def __init__(self, body="", to_sent="", recieved_from=""):
+        self.body = body
         self.recieved_from = ""
-        self.to_sent = ""
-    
+        self.to_sent = to_sent
+
     def fill_content(self,response):
         self.body = response['Body']
         self.recieved_from = response['From']
         self.process_message()
 
-    def process_message(self):
-        data = self.body.split("#$#$#")
+    def inform_other_person(self):
+        emergency_contact = database_interaction.get_emergency_contact(self.recieved_from)
+        self.body = database_interaction.get_name_location( self.recieved_from)
+        self.to_sent = emergency_contact
+        self.send_message()
 
+    def send_message(self):
+        message = Message.client.messages\
+                        .create(body = self.body,
+                                from_ = Message.MOBILE_NUMBER,
+                                to = self.to_sent
+                            )
+        print(message.sid)
+
+    def process_message(self):
+        data = self.body.split(Message.seperator)
+        longitude = ""
+        lattitude = ""
+        no_of_people = ""
         if not data[0]:
             data = data[1:] 
-            location = data[0].split(',')
+            location = data[0].split(",")
+            lattitude = location[0]
+            longitude = location[1]
             no_of_people = data[1]
-            database_data = {'lattitude':location[0],'longitude':location[1]}
-            database_data['no_of_people'] = no_of_people
-            database_data['mo_number'] = self.recieved_from
-            database_interaction.insert("AffectedPeople", **database_data)
-
             self.inform_other_person()
+            
+        else:
+            location_text = data[0]
+            maps = GoogleMapsInteraction.GoogleMaps
+            lattitude, longitude = maps.getLatLngFromText(location_text)
+            no_of_people = data[1]
+        
+        database_data = {'lattitude':lattitude,'longitude':longitude}
+        database_data['no_of_people'] = no_of_people
+        database_data['mo_number'] = self.recieved_from
+        database_interaction.insert("AffectedPeople", **database_data)
 
     #print(message.sid)
